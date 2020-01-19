9663e90b14b0564ed2e93e3d4ea6872ea9606bc1
Kelly Selden <kellyselden@gmail.com>
2020-01-19 23:48:03 +0000

    1.0.18

--- dist/index.js
+++ dist/index.js
@@ -715,65 +715,9 @@ function run(bin, args, options) {
 
 (async() => {
   try {
-    let email = (await run('git', [
-      'show',
-      '-s',
-      '--format=%ae'
-    ])).stdout;
-
-    let name = (await run('git', [
-      'show',
-      '-s',
-      '--format=%ae'
-    ])).stdout;
-
-    let gitEmail = core.getInput('git_email');
-    let gitName = core.getInput('git_name');
-
-    // if (email === gitEmail) {
-    //   console.log('This is the second commit.');
-    //   return;
-    // }
-
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-
-    // let ref = github.context.payload.pull_request.head.ref;
-    // console.log({ ref });
-
-    // await run('git', [
-    //   'remote',
-    //   'remove',
-    //   'origin'
-    // ]);
-
-    // let username = core.getInput('username');
-    // process.env.GITHUB_TOKEN = core.getInput('token');
-
-    // let remote = github.context.payload.repository.clone_url;
-    // console.log({ remote });
-
-    // remote = remote.replace('https://', `https://${username}:$GITHUB_TOKEN@`);
-
-    // await run('git', [
-    //   'remote',
-    //   'add',
-    //   'origin',
-    //   remote
-    // ]);
-
-    // await run('git', [
-    //   'fetch',
-    //   'origin',
-    //   ref
-    // ]);
-
-    // await run('git', [
-    //   'checkout',
-    //   '--track',
-    //   `origin/${ref}`
-    // ]);
 
     await run('npx', [
       'ember-cli-update',
@@ -800,23 +744,42 @@ function run(bin, args, options) {
       '--fix'
     ]);
 
-    await run('git', [
-      'add',
-      '-A'
-    ]);
+    let gitEmail = core.getInput('git_email');
+    let gitName = core.getInput('git_name');
+
+    if (!gitEmail) {
+      gitEmail = (await run('git', [
+        'show',
+        '-s',
+        '--format=%ae'
+      ])).stdout;
+    }
+
+    if (!gitName) {
+      gitName = (await run('git', [
+        'show',
+        '-s',
+        '--format=%an'
+      ])).stdout;
+    }
 
     await run('git', [
       'config',
       '--global',
       'user.email',
-      `"${email}"`
+      `"${gitEmail}"`
     ]);
 
     await run('git', [
       'config',
       '--global',
       'user.name',
-      `"${name}"`
+      `"${gitName}"`
+    ]);
+
+    await run('git', [
+      'add',
+      '-A'
     ]);
 
     await run('git', [
@@ -829,8 +792,8 @@ function run(bin, args, options) {
       'push',
       '-f'
     ]);
-  } catch (error) {
-    core.setFailed(error.message);
+  } catch (err) {
+    core.setFailed(err.message);
   }
 })();
 
