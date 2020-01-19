300dd469c2e4aa641d1ad310adc79a195764a276
Kelly Selden <kellyselden@gmail.com>
2020-01-19 22:48:02 +0000

    test

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ function run(bin, args, options) {
 
     await run('git', [
       'commit',
-      '-m',
-      '"@kellyselden/node-template"'
+      '--amend',
+      '--no-edit'
     ]);
 
     await run('git', [
