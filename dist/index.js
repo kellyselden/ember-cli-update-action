bf5b530998faf44f20855d375da197080a844118
Kelly Selden <kellyselden@gmail.com>
2020-01-19 20:28:41 +0000

    1.0.4

--- dist/index.js
+++ dist/index.js
@@ -745,6 +745,10 @@ function run(bin, args, options) {
       '-A'
     ]);
 
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
     await run('git', [
       'config',
       '--global',
