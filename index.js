71e004a72500fbc4fe419278f8484a2ac20f245a
Kelly Selden <kellyselden@gmail.com>
2020-01-19 21:28:22 +0000

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const execa = require('execa');
 
-function run(bin, args, options) {
+function run(bin, args, options = {}) {
   console.log([bin, ...args].join(' '), options);
 
   let ps = execa(bin, args, {
@@ -30,6 +30,25 @@ function run(bin, args, options) {
       return;
     }
 
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    let ref = github.context.payload.pull_request.head.ref;
+    console.log({ ref });
+
+    await run('git', [
+      'fetch',
+      'origin',
+      ref
+    ]);
+
+    await run('git', [
+      'checkout',
+      '--track',
+      `origin/${ref}`
+    ]);
+
     await run('npx', [
       'ember-cli-update',
       '-b=@kellyselden/node-template'
@@ -51,25 +70,6 @@ function run(bin, args, options) {
       '-A'
     ]);
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
-    let ref = github.context.payload.pull_request.head.ref;
-    console.log({ ref });
-
-    await run('git', [
-      'fetch',
-      'origin',
-      ref
-    ]);
-
-    await run('git', [
-      'checkout',
-      '--track',
-      `origin/${ref}`
-    ]);
-
     await run('git', [
       'config',
       '--global',
@@ -88,6 +88,26 @@ function run(bin, args, options) {
       'commit',
       '-m',
       '"@kellyselden/node-template"'
+    ]);
+
+    await run('git', [
+      'remote',
+      'remove',
+      'origin'
+    ]);
+
+    process.env.GITHUB_TOKEN = core.getInput('GitHubToken');
+
+    let remote = github.context.payload.repository.clone_url;
+    console.log({ remote });
+
+    remote = remote.replace('https://', 'https://$GITHUB_TOKEN@');
+
+    await run('git', [
+      'remote',
+      'add',
+      'origin',
+      remote
     ]);
 
     await run('git', [
