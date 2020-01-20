160c4dc729f816582f994f819b423a2cc77c12e4
Kelly Selden <kellyselden@gmail.com>
2020-01-20 12:14:37 +0000

    test

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function run(bin, args, options) {
 
     console.log({ body });
 
+    // renovate style
     let matches = body.match(/^\| \[([^ ]+)\][^ ]*.*\[`(.+)` -> `(.+)`\]/m);
 
     if (!matches) {
@@ -45,7 +46,7 @@ function run(bin, args, options) {
       packageName
     ])).stdout;
 
-    if (!stats.includes(`${packageName}, current: ${from}, latest: ${to}`)) {
+    if (stats !== `${packageName}, current: ${from}, latest: ${to}`) {
       console.log('not a blueprint match');
       return;
     }
