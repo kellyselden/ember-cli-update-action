5bd529121e4278bb34dbde95f0777adafff1b872
Kelly Selden <kellyselden@gmail.com>
2020-01-20 12:21:58 +0000

    1.0.26

--- dist/index.js
+++ dist/index.js
@@ -727,6 +727,11 @@ function run(bin, args, options) {
     let matches = body.match(/^\| \[([^ ]+)\][^ ]*.*\[`(.+)` -> `(.+)`\]/m);
 
     if (!matches) {
+      // dependabot style
+      matches = body.match(/^Bumps \[(.+)\].* from (.+) to (.+)\.$/m);
+    }
+
+    if (!matches) {
       console.log('not a blueprint');
       return;
     }
