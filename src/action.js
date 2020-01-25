860d8aaf2a1c08c16be2aa48375394954c939cfe
Kelly Selden <kellyselden@gmail.com>
2020-01-25 10:17:13 +0000

    chore: refactor into src dir

---
+++ src/action.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const emberCliUpdateAction = require('.');
+
+(async() => {
+  try {
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+
+    let { body } = github.context.payload.pull_request;
+
+    let installCommand = core.getInput('install_command');
+
+    let autoFixCommand = core.getInput('autofix_command');
+
+    let gitEmail = core.getInput('git_email');
+    let gitName = core.getInput('git_name');
+
+    let amend = core.getInput('amend');
+
+    await emberCliUpdateAction({
+      body,
+      installCommand,
+      autoFixCommand,
+      gitEmail,
+      gitName,
+      amend
+    });
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+})();
